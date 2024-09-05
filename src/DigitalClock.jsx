780df1907f56ea81ebadce369b4fd4e93ab10fb2
sggyflt <sgflt88@gmail.com>
2024-09-05 14:36:24 +0200

    style: add basic styling

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -2,9 +2,9 @@ import { useState, useEffect } from "react";
 
 function DigitalClock() {
   return(
-    <div>
-      <div>
-        <span>00:00:00</span>
+    <div className="h-screen flex justify-center items-center">
+      <div className="backdrop-blur-sm bg-black/25 p-12 w-screen flex justify-center rounded-xl">
+        <span className="text-9xl text-white">00:00:00</span>
       </div>
     </div>
   );
