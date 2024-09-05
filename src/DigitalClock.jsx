a2176790be31b89859f7d14e96a73487ea59e1a9
sggyflt <sgflt88@gmail.com>
2024-09-05 14:55:29 +0200

    feat: add digital clock functionality

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,10 +1,32 @@
 import { useState, useEffect } from "react";
 
 function DigitalClock() {
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, []);
+
+  function formatTime() {
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+    let meridiem = hours >=12 ? "PM" : "AM";
+
+    return `${hours}:${minutes}:${seconds} ${meridiem}`;
+  }
+
   return(
     <div className="h-screen flex justify-center items-center">
       <div className="backdrop-blur-sm bg-black/25 p-12 w-screen flex justify-center rounded-xl">
-        <span className="text-9xl text-white">00:00:00</span>
+        <span className="text-9xl text-white">{formatTime()}</span>
       </div>
     </div>
   );
